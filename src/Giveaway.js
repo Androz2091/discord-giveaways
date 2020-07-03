40849461c2c2aee976b0bbfeebaebf053ddd8160
Androz <androz2091@gmail.com>
2020-07-03 18:06:30 +0200

    Add missing comma

--- src/Giveaway.js
+++ src/Giveaway.js
@@ -255,7 +255,7 @@ class Giveaway extends EventEmitter {
                 message = await this.channel.fetchMessage(this.messageID).catch(() => {});
             }
             if (!message) {
-                this.manager.deleteGiveaway(this.messageID)
+                this.manager.deleteGiveaway(this.messageID);
                 return reject('Unable to fetch message with ID ' + this.messageID + '.');
             }
             this.message = message;
