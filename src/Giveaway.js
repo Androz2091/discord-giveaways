1304eb6c38071dad072588937d4e9dc713362f81
Androz2091 <androz2091@gmail.com>
2020-08-12 15:59:35 +0200

    :bug:  indeed it couldn't work

--- src/Giveaway.js
+++ src/Giveaway.js
@@ -341,13 +341,13 @@ class Giveaway extends EventEmitter {
      */
     end(){
         return new Promise(async (resolve, reject) => {
-            this.ended = true;
             if (this.ended) {
                 return reject('Giveaway with message ID ' + this.messageID + ' is already ended');
             }
             if (!this.channel) {
                 return reject('Unable to get the channel of the giveaway with message ID ' + this.messageID + '.');
             }
+            this.ended = true;
             await this.fetchMessage().catch(() => {});
             if (!this.message) {
                 return reject('Unable to fetch message with ID ' + this.messageID + '.');
