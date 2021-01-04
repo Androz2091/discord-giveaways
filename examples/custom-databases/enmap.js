c160d09fc6751d36e41e8322d9db28cecf27e94a
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-01-04 17:08:10 +0100

    Update examples/custom-databases/enmap.js
    Co-authored-by: Nico <63612668+Nico105@users.noreply.github.com>

--- examples/custom-databases/enmap.js
+++ examples/custom-databases/enmap.js
@@ -28,6 +28,7 @@ const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
     // This function is called when a giveaway needs to be edited in the database.
     async editGiveaway(messageID, giveawayData) {
         giveawayDB.set(messageID, giveawayData);
+        // Don't forget to return something!
         return true;
     }
 
