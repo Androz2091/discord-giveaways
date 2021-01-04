853b45b68711e096d3df5ec6fa68701fd7e05e38
Nico <63612668+Nico105@users.noreply.github.com>
2021-01-04 12:24:56 +0100

    Remove space

--- examples/sharding.js
+++ examples/sharding.js
@@ -30,7 +30,7 @@ const manager = new GiveawayManagerWithShardSupport(client, {
 client.giveawaysManager = manager;
 
 client.on('ready', () => {
-    console.log('I\'m ready !');
+    console.log('I\'m ready!');
 });
 
 client.login(settings.token);
