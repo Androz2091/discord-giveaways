2d3fcdbe7a04cd9538992c44d9eeba87a5cf55a8
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-01-04 17:09:00 +0100

    Update examples/custom-databases/enmap.js
    Co-authored-by: Nico <63612668+Nico105@users.noreply.github.com>

--- examples/custom-databases/enmap.js
+++ examples/custom-databases/enmap.js
@@ -14,7 +14,7 @@ const giveawayDB = new Enmap({ name: 'giveaways' })
 const { GiveawaysManager } = require('discord-giveaways');
 const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
     // This function is called when the manager needs to get all the giveaways stored in the database.
-    async getAllGiveaways(){
+    async getAllGiveaways() {
         return giveawayDB.fetchEverything().array();
     }
 
