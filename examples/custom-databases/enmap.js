bb600a767c0c0e743001fbb8cae62f3fe48b120a
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-01-04 15:10:09 +0100

    Add Enmap as custom database
    I had tried to do the same system as quick.db when I had Enmap, but I noticed that it was not the same system (the base gets corrupted when adding). So I modified and found this solution for those who use [Enmap](https://enmap.evie.dev/).

---
+++ examples/custom-databases/enmap.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client(),
+    settings = {
+        prefix: 'g!',
+        token: 'Your Discord Token'
+    };
+
+// Load Enmap
+const Enmap = require('enmap');
+
+// Create giveaways table
+const giveawayDB = new Enmap({name: "giveaways"})
+
+const { GiveawaysManager } = require('discord-giveaways');
+const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
+
+    // This function is called when the manager needs to get all the giveaway stored in the database.
+    async getAllGiveaways(){
+        return giveawayDB.fetchEverything().array();
+    }
+
+    // This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
+    async saveGiveaway(messageID, giveawayData){
+        giveawayDB.set(messageID, giveawayData);
+        return true;
+    }
+
+    async editGiveaway(messageID, giveawayData){
+        giveawayDB.set(messageID, giveawayData);
+        return true;
+    }
+
+    // This function is called when a giveaway needs to be deleted from the database.
+    async deleteGiveaway(messageID){
+        giveawayDB.delete(messageID);
+        return true;
+    }
+};
+
+// Create a new instance of your new class
+const manager = new GiveawayManagerWithOwnDatabase(client, {
+    storage: false, // Important - use false instead of a storage path
+    updateCountdownEvery: 10000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: [ 'MANAGE_MESSAGES', 'ADMINISTRATOR' ],
+        embedColor: '#FF0000',
+        reaction: '🎉'
+    }
+});
+// We now have a giveawaysManager property to access the manager everywhere!
+client.giveawaysManager = manager;
+
+client.on('ready', () => {
+    console.log('I\'m ready !');
+});
+
+client.login(settings.token);
