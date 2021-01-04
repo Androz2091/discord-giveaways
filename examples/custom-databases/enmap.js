392c7d8d966ad1716e032c448b0829a8fe2b0176
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-01-04 17:07:22 +0100

    Update examples/custom-databases/enmap.js
    Co-authored-by: Nico <63612668+Nico105@users.noreply.github.com>

--- examples/custom-databases/enmap.js
+++ examples/custom-databases/enmap.js
@@ -53,7 +53,7 @@ const manager = new GiveawayManagerWithOwnDatabase(client, {
 client.giveawaysManager = manager;
 
 client.on('ready', () => {
-    console.log('I\'m ready !');
+    console.log('I\'m ready!');
 });
 
 client.login(settings.token);
