c274ff5e21d8841c05a4cc74359fe64088c71932
Nico <63612668+Nico105@users.noreply.github.com>
2021-01-04 12:22:17 +0100

    Remove space

--- examples/custom-databases/mongodb.js
+++ examples/custom-databases/mongodb.js
@@ -70,7 +70,7 @@ const manager = new GiveawayManagerWithOwnDatabase(client, {
 client.giveawaysManager = manager;
 
 client.on('ready', () => {
-    console.log('I\'m ready !');
+    console.log('I\'m ready!');
 });
 
 client.login(settings.token);
