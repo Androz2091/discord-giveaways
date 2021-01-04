3b69d39c238da8980c0d92f93961f98dad08bbe3
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-01-04 17:06:57 +0100

    Update examples/custom-databases/enmap.js
    Co-authored-by: Nico <63612668+Nico105@users.noreply.github.com>

--- examples/custom-databases/enmap.js
+++ examples/custom-databases/enmap.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js'),
     client = new Discord.Client(),
     settings = {
         prefix: 'g!',
-        token: 'Your Discord Token'
+        token: 'Your Discord Bot Token'
     };
 
 // Load Enmap
