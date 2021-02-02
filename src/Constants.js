b676177fc03d55b4e5dcee696978403fd6e4a05a
Nico105 <63612668+Nico105@users.noreply.github.com>
2021-02-02 11:16:46 +0100

    Update error message

--- src/Constants.js
+++ src/Constants.js
@@ -123,7 +123,7 @@ exports.defaultRerollOptions = {
     winnerCount: null,
     messages: {
         congrat: ':tada: New winner(s) : {winners}! Congratulations!\n{messageURL}',
-        error: 'No valid participations, no winners can be chosen!'
+        error: 'No valid participations, no new winner(s) can be chosen!'
     }
 };
 
