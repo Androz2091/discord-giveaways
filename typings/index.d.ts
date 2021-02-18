bd1256572d6bc94e2d18fc1bd1bda70e2d33f277
Androz2091 <androz2091@gmail.com>
2021-02-18 15:34:52 +0100

    :label: Fix typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -41,6 +41,12 @@ declare module 'discord-giveaways' {
 
         public emit<K extends keyof GiveawaysManagerEvents>(event: K, ...args: GiveawaysManagerEvents[K]): boolean;
     }
+    interface LastChanceOptions {
+        enabled: boolean;
+        embedColor: string;
+        content: string;
+        threshold: number;
+    }
     interface GiveawaysManagerOptions {
         storage?: string;
         updateCountdownEvery?: number;
@@ -61,6 +67,7 @@ declare module 'discord-giveaways' {
         reaction?: string;
         messages?: Partial<GiveawaysMessages>;
         extraData?: any;
+        lastChance?: LastChanceOptions;
     }
     interface GiveawaysMessages {
         giveaway?: string;
@@ -89,18 +96,11 @@ declare module 'discord-giveaways' {
     class Giveaway extends EventEmitter {
         constructor(manager: GiveawaysManager, options: GiveawayData);
 
-        public botsCanWin: boolean;
-        readonly channel: TextChannel;
         public channelID: Snowflake;
         public client: Client;
-        readonly content: string;
         public data: GiveawayData;
-        public embedColor: ColorResolvable;
-        public embedColorEnd: ColorResolvable;
         public endAt: number;
         public ended: boolean;
-        public exemptPermissions: PermissionResolvable[];
-        readonly giveawayDuration: number;
         public guildID: Snowflake;
         public hostedBy: string | null;
         public manager: GiveawaysManager;
@@ -109,11 +109,23 @@ declare module 'discord-giveaways' {
         public messages: GiveawaysMessages;
         public options: GiveawayData;
         public prize: string;
-        readonly remainingTime: number;
-        readonly messageURL: string;
         public startAt: number;
         public winnerCount: number;
         public winnerIDs: Array<string>;
+
+        // getters calculated using default manager options
+        readonly exemptPermissions: PermissionResolvable[];
+        readonly giveawayDuration: number;
+        readonly embedColor: ColorResolvable;
+        readonly embedColorEnd: ColorResolvable;
+        readonly botsCanWin: boolean;
+        readonly reaction: string;
+
+        // getters calculated using other values
+        readonly remainingTime: number;
+        readonly messageURL: string;
+        readonly content: string;
+        readonly channel: TextChannel;
 
         public exemptMembers(): boolean;
         public edit(options: GiveawayEditOptions): Promise<Giveaway>;
