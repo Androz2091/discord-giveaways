e1bdf71216042ae64c166ed9cb87fc7a85d35cc7
Lebyy <45114019+Lebyy@users.noreply.github.com>
2021-02-27 19:11:09 +0530

    Update src/Giveaway.js
    Co-authored-by: Nico <63612668+Nico105@users.noreply.github.com>

--- src/Giveaway.js
+++ src/Giveaway.js
@@ -470,7 +470,7 @@ class Giveaway extends EventEmitter {
             if (Array.isArray(options.newBonusEntries) && options.newBonusEntries.every((elem) => typeof elem === 'object'))
                 this.options.bonusEntries = options.newBonusEntries;
             if (options.newExtraData) this.extraData = options.newExtraData;
-            if(typeof options.newisDrop === 'boolean') this.isDrop = options.newisDrop;
+            if (typeof options.newIsDrop === 'boolean') this.options.isDrop = options.newIsDrop;
             // Call the db method
             await this.manager.editGiveaway(this.messageID, this.data);
             resolve(this);
