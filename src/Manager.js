5537bac3d2362f9351bba2dd2115989b84299055
Lebyy <45114019+Lebyy@users.noreply.github.com>
2021-02-27 19:15:27 +0530

    Update src/Manager.js
    Co-authored-by: Nico <63612668+Nico105@users.noreply.github.com>

--- src/Manager.js
+++ src/Manager.js
@@ -410,7 +410,7 @@ class GiveawaysManager extends EventEmitter {
                 await this.editGiveaway(giveaway.messageID, giveaway.data);
                 return;
             }
-            if (giveaway.options.isDrop == true && giveaway.message.reactions.cache.get(giveaway.reaction).count - 1 >= giveaway.winnerCount) {
+            if (giveaway.isDrop && giveaway.message.reactions.cache.get(giveaway.reaction).count - 1 >= giveaway.winnerCount) {
                 return this.end(giveaway.messageID).catch(() => {});
             }
             const embed = this.generateMainEmbed(giveaway, giveaway.lastChance.enabled && giveaway.remainingTime < giveaway.lastChance.threshold);
