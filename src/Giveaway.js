4508c5399c3761c0c67358f47df9dbe984a9c9d1
Nico105 <63612668+Nico105@users.noreply.github.com>
2021-07-09 12:55:25 +0200

    remove space

--- src/Giveaway.js
+++ src/Giveaway.js
@@ -549,7 +549,7 @@ class Giveaway extends EventEmitter {
                 this.winnerIDs = winners.map((w) => w.id);
                 await this.manager.editGiveaway(this.messageID, this.data);
                 const embed = this.manager.generateEndEmbed(this, winners);
-                await this.message.edit({ content: this.messages.giveawayEnded, embeds : [embed] }).catch(() => {});
+                await this.message.edit({ content: this.messages.giveawayEnded, embeds: [embed] }).catch(() => {});
                 let formattedWinners = winners.map((w) => `<@${w.id}>`).join(', ');
                 const messageString = this.messages.winMessage
                     .replace('{winners}', formattedWinners)
