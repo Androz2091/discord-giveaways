a42a865e5bca477f9a73cab6830e75566a1d3765
Nico105 <63612668+Nico105@users.noreply.github.com>
2021-08-02 21:26:41 +0200

    initial

---
+++ examples/custom-databases/nano.js
@@ -0,0 +1,74 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] }),
+    settings = {
+        prefix: 'g!',
+        token: 'Your Discord Bot Token'
+    };
+
+// Load nano
+const nano = require('nano')('http://admin:mypassword@localhost:5984');
+let giveawayDB;
+
+const { GiveawaysManager } = require('discord-giveaways');
+const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
+    // This function is called when the manager needs to get all giveaways which are stored in the database.
+    async getAllGiveaways() {
+        // Get all giveaways from the database
+        return (await giveawayDB.list({ include_docs: true })).rows.map((r) => r.doc);
+    }
+
+    // This function is called when a giveaway needs to be saved in the database.
+    async saveGiveaway(messageID, giveawayData) {
+        // Add the new giveaway to the database
+        await giveawayDB.insert(giveawayData, messageID);
+        // Don't forget to return something!
+        return true;
+    }
+
+    // This function is called when a giveaway needs to be edited in the database.
+    async editGiveaway(messageID, giveawayData) {
+        // Get the unedited giveaway from the database
+        const giveaway = await giveawayDB.get(messageID);
+        // Edit the giveaway
+        await giveawayDB.insert({ ...giveaway, ...giveawayData });
+        // Don't forget to return something!
+        return true;
+    }
+
+    // This function is called when a giveaway needs to be deleted from the database.
+    async deleteGiveaway(messageID) {
+        // Get the giveaway from the database
+        const giveaway = await giveawayDB.get(messageID);
+        // Remove the giveaway from the database
+        await giveawayDB.destroy(messageID, giveaway._rev);
+        // Don't forget to return something!
+        return true;
+    }
+};
+
+// Create a new instance of your new class
+const manager = new GiveawayManagerWithOwnDatabase(client, {
+    updateCountdownEvery: 10000,
+    default: {
+        botsCanWin: false,
+        embedColor: '#FF0000',
+        embedColorEnd: '#000000',
+        reaction: '🎉'
+    }
+}, false); // ATTENTION: Add "false" in order to not start the manager until the DB got checked, see below
+// We now have a giveawaysManager property to access the manager everywhere!
+client.giveawaysManager = manager;
+
+// Check the DB
+(async () => {
+    if (!(await nano.db.list()).includes('giveaways')) await nano.db.create('giveaways');
+    giveawayDB = nano.use('giveaways');
+    // Start the manager only after the DB got checked to prevent an error
+    client.giveawaysManager._init();
+})();
+
+client.on('ready', () => {
+    console.log('I\'m ready!');
+});
+
+client.login(settings.token);
