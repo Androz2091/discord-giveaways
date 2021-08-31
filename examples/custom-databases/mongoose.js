f1076df04110557a0fd723c128d16144686225b9
Nico105 <63612668+Nico105@users.noreply.github.com>
2021-08-31 22:19:02 +0200

    mongoose - improve example
    after all these months - why did the orginal author use methods that do more stuff than needed? XD

--- examples/custom-databases/mongoose.js
+++ examples/custom-databases/mongoose.js
@@ -84,7 +84,7 @@ const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
     // This function is called when the manager needs to get all giveaways which are stored in the database.
     async getAllGiveaways() {
         // Get all giveaways from the database. We fetch all documents by passing an empty condition.
-        return await giveawayModel.find({}).lean().exec();
+        return await giveawayModel.find().lean().exec();
     }
 
     // This function is called when a giveaway needs to be saved in the database.
@@ -98,7 +98,7 @@ const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
     // This function is called when a giveaway needs to be edited in the database.
     async editGiveaway(messageId, giveawayData) {
         // Find by messageId and update it
-        await giveawayModel.findOneAndUpdate({ messageId }, giveawayData, { omitUndefined: true }).lean().exec();
+        await giveawayModel.updateOne({ messageId }, giveawayData, { omitUndefined: true }).exec();
         // Don't forget to return something!
         return true;
     }
@@ -106,7 +106,7 @@ const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
     // This function is called when a giveaway needs to be deleted from the database.
     async deleteGiveaway(messageId) {
         // Find by messageId and delete it
-        await giveawayModel.findOneAndDelete({ messageId }).lean().exec();
+        await giveawayModel.deleteOne({ messageId }).exec();
         // Don't forget to return something!
         return true;
     }
