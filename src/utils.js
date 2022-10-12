d4979ef048fdd653463f4fbb0390108dd97288f6
Nico105 <63612668+Nico105@users.noreply.github.com>
2022-10-12 21:02:16 +0200

    fix embedEqual

--- src/utils.js
+++ src/utils.js
@@ -16,7 +16,6 @@ exports.embedEqual = (embed1, embed2) => {
     if (embed1.description !== embed2.description) return false;
     if (embed1.url !== embed2.url) return false;
     if (embed1.color !== embed2.color) return false;
-    if (Date.parse(embed1.timestamp) !== Date.parse(embed2.timestamp)) return false;
     if (embed1.footer?.text !== embed2.footer?.text) return false;
     if (embed1.footer?.icon_url !== embed2.footer?.icon_url) return false;
     if (embed1.thumbnail?.url !== embed2.thumbnail?.url) return false;
